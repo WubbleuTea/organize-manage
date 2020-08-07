0734ad335479b84cb6a74d78b964ed97db8afd64
Joshua Weisbrod-Torres <64339271+joshuaweisbrodtorres@users.noreply.github.com>
2020-08-06 21:26:31 -0500

    started working on functions that get all from a table

---
+++ server.js
@@ -0,0 +1,48 @@
+const { openingQuestion, addDept, addRole, addEmployee, updateRole } = require('./lib/questions')
+const inquirer = require('inquirer')
+const mysql = require('mysql2');
+const cTable = require('console.table')
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3001,
+    // Your MySQL username
+    user: 'root',
+    // Your MySQL password
+    password: ***REMOVED***,
+    database: 'ice_creamDB'
+});
+// Creates the imag, starts the connection and goes to first list of questions.
+const startUp = () => {
+    console.log('Employee Table')
+    connection.connect(err => {
+        if (err) throw err;
+        console.log('connected as id ' + connection.threadId);
+        listQuestions();
+      });
+
+}
+// list of questions are asked
+const listQuestions = () => {
+    inquirer.prompt(openingQuestion)
+    .then(function(answer) {
+        if (answer == 'view all departments') {
+            viewAllDepartments();
+        }
+       
+    });
+};
+
+// Departments are shown
+const viewAllDepartments = () => {
+    connection.query(
+        'SELECT * FROM `departments`',
+        function(err, results) {
+          if (err) throw err;
+          console.table(results); 
+          listQuestions()
+        }
+      );
+}
+
+startUp()
