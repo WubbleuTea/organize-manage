8cd4b8d9b9dad58d2b6c847a79d8d63bb4dcae62
Joshua Weisbrod-Torres <64339271+joshuaweisbrodtorres@users.noreply.github.com>
2020-08-09 14:23:42 -0500

    using nameId now

--- db/schema.sql
+++ db/schema.sql
@@ -23,7 +23,7 @@ CREATE TABLE roles (
 );
 
 CREATE TABLE employees (
-    id INTEGER AUTO_INCREMENT NOT NULL,
+    id INT AUTO_INCREMENT NOT NULL,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT NOT NULL,
