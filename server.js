135b3283798daf41820d5d70c2c7f223bd95bc85
Joshua Weisbrod-Torres <jwtorres@rocketmail.com>
2020-08-15 09:22:08 -0500

    added dotenv to file and cleaned up adding role

--- server.js
+++ server.js
@@ -2,13 +2,15 @@ const inquirer = require('inquirer')
 const mysql = require('mysql2');
 const cTable = require('console.table')
 const figlet = require('figlet');
+require('dotenv').config();
+
 
 const connection = mysql.createConnection({
     host: 'localhost',
     port: 3306,
-    user: 'root',
-    password: ***REMOVED***,
-    database: ***REMOVED***
+    user: process.env.DB_USER,
+    password: process.env.DB_PW,
+    database: process.env.DB_NAME
 });
 // connects to mysql and begins the program.
 connection.connect(err => {
@@ -28,8 +30,10 @@ startUp = () => {
         listQuestions();
     });
 };
+
 // list of questions are asked
 listQuestions = () => {
+    // 
     inquirer.prompt([
         {
             type: 'list',
@@ -46,6 +50,7 @@ listQuestions = () => {
                 'Quit']
         }
     ])
+    // use the answer choice to continue through the application
     .then(answer => {
         switch (answer.firstQuestion) {
             case 'View All Departments':
@@ -158,10 +163,13 @@ addToDept = () => {
     })
 };
 
-// add a new role
+// Add an role to the database
 async function addToRole() {
     let deptId;
-    let departments = await allDepartments()
+    // array created for the answer choices
+    let departments = await allDepartments();
+
+     // Asks the question for what they want to add
     inquirer.prompt([
         {
             type: 'input',
@@ -197,41 +205,44 @@ async function addToRole() {
         }
     ])
     .then(response => {
+        let { name, salary, depart } = response
         connection.query(
-                `SELECT * FROM departments`,
-                function(err, results) {
-                  if (err) throw err; 
-                    for (let i = 0; i < results.length; i++) {
-                        if (response.depart === results[i].department_name) {
-                            deptId = results[i].id;
-                            connection.query(
-                                `INSERT INTO roles (title, salary, department_id)
-                                VALUES (?,?,?)`,
-                                [
-                                    response.name,
-                                    response.salary,
-                                    deptId
-                                ],
-                                function(err, res) {
-                                    if (err) throw err;
-                                    console.log(res.affectedRows + ' added!\n');
-                                    listQuestions();
-                                }
-                            );
-                        }
+            // query to find the manager using the array that was made
+            `SELECT id FROM departments where department_name = ?`,
+            [ depart ], function(err, results) {
+                if (err) throw err;
+                // set the departmentID for use in the next connection
+                deptId = results[0].id;
+                // Add the new role to the table using the responses and the found id
+                connection.query(
+                    `INSERT INTO roles (title, salary, department_id)
+                    VALUES (?,?,?)`,
+                    [
+                        name,
+                        salary,
+                        deptId
+                    ],
+                    function(err, res) {
+                        if (err) throw err;
+                        console.log(res.affectedRows + ' added!\n');
+                        // Return back to the original set of questions.
+                        listQuestions();
                     }
-                }
+                );
+            }
         );
     });
 };
 
+// Add an employee to the database
 async function addToEmployee() {
     let roleId;
     let managerId;
-    let managerArr = [];
-    let employees = await allEmployees()
-    let roles = await allRoleNames()
+    // arrays created for the answer choices
+    let employees = await allEmployees();
+    let roles = await allRoleNames();
 
+    // Asks the question for what they want to add
     inquirer.prompt([
         {
             type: 'input',
@@ -273,49 +284,56 @@ async function addToEmployee() {
         }
     ])
     .then(response => {
+        // destructure response
         const { firstName, lastName, role, manager } = response
+        //make a manager array to hold the first and last name and split the answered name into an array
+        let managerArr = [];
         managerArr = manager.split(' ');
-        // query to find the manager
+        // query to find the manager using the array that was made
         connection.query(`SELECT id FROM employees WHERE first_name = ? AND last_name = ?`,
             [managerArr[0], managerArr[1]], function (err, results) {  
             if (err) throw err;
+            // set the managerId to that found number to use later.
             managerId = results[0].id;
-                    // Query to find the role id
-                    connection.query(`SELECT id FROM roles WHERE title = ?`,
-                        [ role ], function(err, results) {
-                            if (err) throw err; 
-                            roleId = results[0].id;
-                                //Insert Query for the table.
-                                connection.query(
-                                    `INSERT INTO employees (first_name, last_name, role_id,  manager_id)
-                                    VALUES (?,?,?,?)`,
-                                    [
-                                        firstName,
-                                        lastName,
-                                        roleId,
-                                        managerId
-                                    ],
-                                    function(err, res) {
-                                        if (err) throw err;
-                                        console.log(res.affectedRows + ' added!\n');
-                                        listQuestions();
-                                    }
-                                );
-                        }
-                    );    
+                /// Query that takes the title name and finds the id where it matches
+                connection.query(`SELECT id FROM roles WHERE title = ?`,
+                    [ role ], function(err, results) {
+                    if (err) throw err; 
+                    // set the roleId to use in the next connection
+                    roleId = results[0].id;
+                        //Insert Query for the table using the response firstName and LastName, and the two set variables.
+                        connection.query(
+                            `INSERT INTO employees (first_name, last_name, role_id,  manager_id)
+                            VALUES (?,?,?,?)`,
+                            [
+                                firstName,
+                                lastName,
+                                roleId,
+                                managerId
+                            ],
+                            function(err, res) {
+                                if (err) throw err;
+                                console.log(res.affectedRows + ' added!\n');
+                                // Return back to the original set of questions.
+                                listQuestions();
+                            }
+                        );
+                    }
+                );    
             }
         );
     });
 };
 
-// 
-
+// Updates an employees roles
 async function updateRoleFunct()  {
     let nameId;
     let roleId;
-    let employees = await allEmployees()
-    let roles = await allRoleNames()
-    
+    // arrays created for the answer choices
+    let employees = await allEmployees();
+    let roles = await allRoleNames();
+
+    // Asks the question of what to change
     inquirer.prompt([
         {
             type: 'list',
@@ -329,16 +347,23 @@ async function updateRoleFunct()  {
             message: 'Please enter the new role of this employee.',
             choices: roles
         }
+        //find the id of the person that has been requested to update
     ]).then(response => {
+        // split the name into an array us it for MySQL
       let nameArr = response.name.split(" ");
+      // find the emplyee in the table
         connection.query(`SELECT id FROM employees WHERE first_name = ? AND last_name = ?`,
-            [nameArr[0], nameArr[1]], function (err, results) {
+            [ nameArr[0], nameArr[1] ], function (err, results) {
                 if (err) throw err;
+                // set the nameID to use in the actual UPDATE
                 nameId = results[0].id;            
+                    // Query that takes the title name and finds the id where it matches
                     connection.query(`SELECT id FROM roles WHERE title = ?`,
                         [ response.role ], function(err, results) {
                             if (err) throw err; 
+                            // set the roleId to use in the next connection
                             roleId = results[0].id;
+                            // update the employee role at the specific employee id
                                 connection.query(
                                     `UPDATE employees SET role_id = ? WHERE id = ?`,
                                     [
@@ -348,6 +373,7 @@ async function updateRoleFunct()  {
                                     function (err, res) {
                                         if (err) throw err;
                                         console.log(res.affectedRows + " updated!\n");
+                                        // Return back to the original set of questions.
                                         listQuestions();
                                     }
                                 );
